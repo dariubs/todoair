df840ebed3674943fca8c1bbda367d265e5162d0
dariush <dariushem@yahoo.com>
2014-04-19 10:58:05 +0430

    line-through on checked items

--- static/assets/todoair.js
+++ static/assets/todoair.js
@@ -6,13 +6,17 @@ angular.module('todoair',[])
  
  	/*Add to todoList */
 	$scope.add = function() {
-	  	$scope.list.push({todo:$scope.todoInput,time:$scope.timeInput, done:0});
+	  	$scope.list.push({todo:$scope.todoInput,time:$scope.timeInput, done:false});
 	};
 
 	/*remove from todolist */
 	$scope.remove = function(i){
 		$scope.list.splice(i,1);
 	};
+
+	$scope.did = function(i){
+		$scope.list()
+	}
 });
 
 
